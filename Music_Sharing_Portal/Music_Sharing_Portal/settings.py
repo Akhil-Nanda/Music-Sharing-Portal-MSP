b01d0203927adde78f6302daff85c1ad2849aa6d
Akhil Nanda N <126058908+Akhil-Nanda@users.noreply.github.com>
2023-06-22 14:58:10 +0530

    Update settings.py

--- Music_Sharing_Portal/Music_Sharing_Portal/settings.py
+++ Music_Sharing_Portal/Music_Sharing_Portal/settings.py
@@ -132,7 +132,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'akhilnanda54@gmail.com'
-EMAIL_HOST_PASSWORD = 'ypznwshmujwwngia'
+EMAIL_HOST_USER = 'enter host email id'
+EMAIL_HOST_PASSWORD = 'enter host email password'
 
 
